12f12591b920ca0d1d5b04d5ca5e3854771e9abf
Artem Netsvetaev <netsvetaev.mgn@gmail.com>
2020-12-08 16:48:43 +0300

    create header and content with data

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div id="app">
-
+    <Header />
+    <Content
+      v-bind:papers='papers'
+    />
   </div>
 </template>
 
 <script>
-
-
+import Header from '@/components/Header.vue'
+import Content from '@/components/Content.vue'
 export default {
   name: 'App',
+  data() {
+    return {
+      papers: [
+        {paperTitle: "Initial Paper", paperAuthor: "physfeel", paperDate: "2020/12/8"}
+      ]
+    }
+  },
   components: {
-
+    Header, Content
   }
 }
 </script>
 
 <style>
-
+* {
+  margin: 0;
+}
 </style>
